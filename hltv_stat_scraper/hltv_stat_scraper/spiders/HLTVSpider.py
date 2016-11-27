c1abc96ae02798160a5e6bdfc7985d63319a1bf6
trsmith <trsmith@iastate.edu>
2016-11-27 15:32:16 +0100

    Can now scrape two specified team pages.

--- hltv_stat_scraper/hltv_stat_scraper/spiders/HLTVSpider.py
+++ hltv_stat_scraper/hltv_stat_scraper/spiders/HLTVSpider.py
@@ -6,12 +6,38 @@ class HLTVSpider(scrapy.Spider):
     name = "hltv"
     #team page min 29 maps, last 3 months
     start_urls = ['http://www.hltv.org/?pageid=182&statsfilter=5']
-    base_url = "http://www.hltv.org"
+
+    def __init__(self):
+        self.base_url = "http://www.hltv.org"
+        self.team1 = '"Astralis"'
+        self.team2 = '"G2"'
+        self.parse(self)
 
     def parse(self, response):
-        team_link = self.base_url + response.xpath('//a[@href="/?pageid=179&teamid=6773&statsfilter=5"]/@href').extract_first()
+        team_links = []
+        self.add_team_link(team_links, self.team1, response)
+        self.add_team_link(team_links, self.team2, response)
 
-        yield scrapy.Request(team_link, callback=self.parseTeam)
+        for link in team_links:
+            yield scrapy.Request(link, callback=self.parseTeam)
+
+    def add_team_link(self, team_links, team, response):
+        team_link_xpath = '//a[contains(.,%s)]/@href' % team
+        team_link = self.base_url + response.xpath(team_link_xpath).extract_first()
+        team_links.append(team_link)
 
     def parseTeam(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+        print "Yee"
+
+        #players = response.xpath('(//div[@class="covGroupBoxContent"])[2]/div[@class="covSmallHeadline"]/text()').extract_first()
+        player_box_content = response.xpath('//div[@class="covGroupBoxContent"][2]')
+        players = player_box_content.xpath('.//div[@class="covSmallHeadline"]//text()').extract()
+        for player in players:
+            print player
+
         print "Yay!"
